a9d62e6de0a7626b00735470e974f5455ff349a6
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2023-03-19 13:40:32 +0100

    Implement LocalSearch tests

--- combinatorial/trajectory/local_search.py
+++ combinatorial/trajectory/local_search.py
@@ -1,61 +1,46 @@
-from typing import Callable, Union
+import abc
+from abc import ABCMeta
 from combinatorial.trajectory.utils.helpers import imp_best, imp_first
-from combinatorial.trajectory.base import NeighborhoodStructure
 
 
-class LocalSearch:
-    def __init__(self,
-                 objective_fn: Callable,
-                 nbhd_struct: NeighborhoodStructure,
-                 improve_fn: Union[str, Callable] = "first",
-                 ):
-        """
-
-        Args:
-            objective_fn: The objective function to be optimized
-            nbhd_struct: The neighborhood stucture
-            improve_fn: The improvement function (right now only 'first' and 'best' available)
-        """
-        self.objective_fn = objective_fn
-        self.nbhd_struct = nbhd_struct
-        self.improve_fn = improve_fn
+class LocalSearch(metaclass=ABCMeta):
+    """
+    Implementation of LocalSearch algorithm
+    """
+    def __init__(self, init_sol, improve_strategy="first"):
+        self.init_sol = init_sol
+        self.improve_strategy = improve_strategy
 
     def improve(self, curr, neighbors: list):
-        if self.improve_fn == "first":
+        if self.improve_strategy == "first":
             return imp_first(self.objective_fn, curr, neighbors)
-        elif self.improve_fn == "best":
+        elif self.improve_strategy == "best":
             return imp_best(self.objective_fn, curr, neighbors)
         else:
-            return self.improve_fn(self.objective_fn, curr, neighbors)
+            raise NotImplementedError("improve_mehod must be one of ['first', 'best']")
 
-    def search(self, init_sol, max_iter: int = 1000):
+    @abc.abstractmethod
+    def objective_fn(self, sol):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_neighbors(self, sol):
+        raise NotImplementedError
+
+    def search(self, max_iter: int = 1000):
         """
         Args:
             init_sol: The initial solution
             max_iter: Maximum iterations before stopping the algorithm
         """
-        curr = init_sol
+        curr = self.init_sol
         for _ in range(max_iter):
             candidate, cost = self.improve(
                 curr,
-                self.nbhd_struct.get_neighbors(curr)
+                self.get_neighbors(curr)
             )
             if cost > self.objective_fn(curr):
                 curr = candidate
             else:
                 return curr
         return curr
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ tests/test_combinatorial/test_trajectory/test_local_search.py
@@ -0,0 +1,55 @@
+import numpy as np
+from copy import deepcopy
+from combinatorial.trajectory.local_search import LocalSearch
+
+
+def test_local_search():
+    """
+    Solve an instance of Knapsack problem
+    """
+    class KnapsackProblem(LocalSearch):
+        """
+        Implementation of Knapsack problem as in Wikipedia example: https://en.wikipedia.org/wiki/Knapsack_problem
+        """
+        # Order list of values according to weight
+        LIST_OF_VALUES = [1, 2, 2, 10, 4]
+        LIST_OF_WEIGHTS = [1, 1, 2, 4, 12]
+        TOTAL_CAPACITY = 15
+
+        # We treat each solution as an array of booleans. 1 means the object is inside the knapsack, 0 that it isn't
+
+        def objective_fn(self, sol):
+            """
+            Computes the total value of all the objects inside the knapsack
+            """
+            total_value = 0
+            for index, in_bag in enumerate(sol):
+                total_value += self.LIST_OF_VALUES[index] * in_bag
+            return total_value
+
+        def get_total_weight(self, sol):
+            total_weight = 0
+            for index, in_bag in enumerate(sol):
+                total_weight += self.LIST_OF_WEIGHTS[index] * in_bag
+            return total_weight
+
+        def get_neighbors(self, sol):
+            """
+            For example, perform a permutation in each element of the array
+            """
+            neighbors = []
+
+            for i in range(len(sol)):
+                neighbor = deepcopy(sol)
+                neighbor[i] = 0 if sol[i] else 1
+
+                if self.get_total_weight(neighbor) <= self.TOTAL_CAPACITY:
+                    neighbors.append(neighbor)
+            return neighbors
+
+    init_sol = [1, 1, 0, 0, 0]
+    knapsack = KnapsackProblem(init_sol=init_sol)
+
+    # The optimal result is picking the two 1kg blocks, the 2 kg block and the 4 kg block.
+    assert knapsack.search(20) == [1, 1, 1, 1, 0]
+
