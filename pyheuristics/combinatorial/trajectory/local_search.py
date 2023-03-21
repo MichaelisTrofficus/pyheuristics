acea0ee10c5d03bedfcb5080ae45041b4fd289d7
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2023-03-21 10:20:31 +0100

    Add first and best strategy to local_search module

--- pyheuristics/combinatorial/trajectory/local_search.py
+++ pyheuristics/combinatorial/trajectory/local_search.py
@@ -1,12 +1,55 @@
 import abc
 from abc import ABCMeta
 from typing import Callable, Union
-from pyheuristics.combinatorial.trajectory.utils.helpers import imp_best, imp_first
 from pyheuristics.execution_result import ExecutionResult
+
+# TODO: Maybe move these two method to another directory in the future.
+
+
+def first_strategy(objective_fn, curr_sol, curr_cost, neighbors):
+    """
+    Selects the first candidate that is better than the best solution found so far.
+
+    Args:
+        objective_fn: The objective function of the optimization problem
+        curr_sol: The current solution
+        curr_cost: The current solution's cost
+        neighbors: The list of neighbors
+
+    Returns:
+        A tuple of (sol, cost)
+    """
+    for sol in neighbors:
+        if objective_fn(sol) < curr_cost:
+            return sol, objective_fn(sol)
+    return curr_sol, curr_cost
+
+
+def best_strategy(objective_fn, curr_sol, curr_cost, neighbors):
+    """
+    Selects the best candidate from all the candidates in the set of neighbors that is
+    also better than the best solution found so far.
+
+    Args:
+        objective_fn: The objective function of the optimization problem
+        curr_sol: The current solution
+        curr_cost: The current solution's cost
+        neighbors: The list of neighbors
+
+    Returns:
+        A tuple of (sol, cost)
+    """
+    best = curr_sol
+    for sol in neighbors:
+        candidate_cost = objective_fn(sol)
+        if candidate_cost < curr_cost:
+            curr_cost = objective_fn(sol)
+            best = sol
+    return best, curr_cost
 
 
 class LocalSearch(metaclass=ABCMeta):
-    def __init__(self, init_sol, improve_strategy: Union[Callable, str] = "first"):
+    def __init__(self, init_sol, strategy: Union[Callable, str] = "first"):
         """
         Implementation of the LocalSearch algorithm
 
@@ -14,7 +57,7 @@ class LocalSearch(metaclass=ABCMeta):
             init_sol: The initial solution, i.e., the starting point of the trajectory. This parameter
                 can be anything: a string, an array, a graph, etc. Basically, the data structure that best
                 represents a solution to the problem.
-            improve_strategy: The type of strategy the algorithm follows when selecting a candidate from
+            strategy: The type of strategy the algorithm follows when selecting a candidate from
                 the set of neighbors. Currently, there are two strategies implemented:
 
                 - 'best': Selecting the best candidate from all the candidates in the set of neighbors that is
@@ -28,26 +71,21 @@ class LocalSearch(metaclass=ABCMeta):
         """
         self.init_sol = init_sol
 
-        if (
-            improve_strategy not in ["first", "best"]
-            and type(improve_strategy) != Callable
-        ):
+        if strategy not in ["first", "best"] and type(strategy) != Callable:
             raise NotImplementedError(
                 "improve_strategy must be one of ['first', 'best']"
             )
 
-        self.improve_strategy = improve_strategy
+        self.strategy = strategy
 
     def improve(self, curr_sol, curr_cost, neighbors: list):
-        if self.improve_strategy == "first":
-            return imp_first(self.objective_fn, curr_sol, curr_cost, neighbors)
-        elif self.improve_strategy == "best":
-            return imp_best(self.objective_fn, curr_sol, curr_cost, neighbors)
+        if self.strategy == "first":
+            return first_strategy(self.objective_fn, curr_sol, curr_cost, neighbors)
+        elif self.strategy == "best":
+            return best_strategy(self.objective_fn, curr_sol, curr_cost, neighbors)
         else:
             # User-defined improve strategies will be called here
-            return self.improve_strategy(
-                self.objective_fn, curr_sol, curr_cost, neighbors
-            )
+            return self.strategy(self.objective_fn, curr_sol, curr_cost, neighbors)
 
     @abc.abstractmethod
     def objective_fn(self, sol):
