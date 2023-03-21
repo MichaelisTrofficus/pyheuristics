b4c3dca87cc5f8a09beb5f4953187f41a6eb2b53
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2023-03-21 13:46:45 +0100

    Add logo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# pyheuristics
+<p align="center">
+  <img alt="Pyheuristics logo" src="img/logo.png" width=300 />
+  <h1 align="center">PyHeuristics</h1>
+  <h3 align="center">A toolkit for MetaHeuristics implemented in Python</h3>
+</p>
+
+---
+
+
 A metaheuristics toolkit implemented in Python
--- pyheuristics/combinatorial/trajectory/simulated_annealing.py
+++ pyheuristics/combinatorial/trajectory/simulated_annealing.py
@@ -1,12 +1,16 @@
 from abc import ABC
-from typing import Callable, Union
 
 from pyheuristics.combinatorial.trajectory.base import TrajectoryMethod
 from pyheuristics.execution_result import ExecutionResult
 
 
 class SimulatedAnnealing(ABC, TrajectoryMethod):
-    def __init__(self, init_sol, strategy: Union[Callable, str] = "first"):
+    def __init__(
+        self,
+        init_sol,
+        t_min: float,
+        t_max: float,
+    ):
         """
         Implementation of the LocalSearch algorithm
 
@@ -14,26 +18,15 @@ class SimulatedAnnealing(ABC, TrajectoryMethod):
             init_sol: The initial solution, i.e., the starting point of the trajectory. This parameter
                 can be anything: a string, an array, a graph, etc. Basically, the data structure that best
                 represents a solution to the problem.
-            strategy: The type of strategy the algorithm follows when selecting a candidate from
-                the set of neighbors. Currently, there are two strategies implemented:
 
-                - 'best': Selecting the best candidate from all the candidates in the set of neighbors that is
-                    also better than the best solution found so far.
+            t_min: Minimum temperature
 
-                - 'first': selecting the first candidate that is better than the best solution found so far.
+            t_max: Maximum temperature
 
-            However, this parameter also accepts a user-defined function as input. This function must have
-            the same signature as the `pyheuristics.combinatorial.trajectory.utils.helpers.imp_best` and
-            `pyheuristics.combinatorial.trajectory.utils.helpers.imp_first` functions.
         """
         self.init_sol = init_sol
-
-        if strategy not in ["first", "best"] and type(strategy) != Callable:
-            raise NotImplementedError(
-                "improve_strategy must be one of ['first', 'best']"
-            )
-
-        self.strategy = strategy
+        self.t_min = t_min
+        self.t_max = t_max
 
     def run(self, max_iter: int = 1000, verbose=0, history=False) -> ExecutionResult:
         """
